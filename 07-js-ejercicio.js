ee1f2bc903e70ef8c297db577821530d0722df10
Santillán D. Renzo <rsantillanc@gmail.com>
2022-10-25 08:24:08 -0500

    first commit

---
+++ 07-js-ejercicio.js
@@ -0,0 +1,18 @@
+const myFamily = new Set()
+myFamily.add("Renzo")
+myFamily.add("Melani")
+myFamily.add("Gioselin")
+myFamily.add("Kenia")
+myFamily.add("Rodrigo")
+myFamily.add("Diego")
+myFamily.add("Carmen")
+myFamily.add("Gael")
+myFamily.add("Ana")
+myFamily.add("Patty")
+
+//Add duplicated name (Me)
+myFamily.add("Renzo")
+//Add Javascrtip
+myFamily.add("Javascript")
+//Print
+console.log(myFamily)
